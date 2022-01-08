948f3cec0da284da451fcfb63ca7b464590fda11
matthieu-dev-code <70697534+matthieu-dev-code@users.noreply.github.com>
2022-01-08 16:52:47 +0100

    Update result.service.spec.ts

--- src/scripts/ts/result/result.service.spec.ts
+++ src/scripts/ts/result/result.service.spec.ts
@@ -10,28 +10,6 @@ describe('ResultService', () => {
   });
 
 
-  /* step 1 : initialisation du projet avec 0 et 1 resultat */
-  it('devrait être initialisé avec une liste de résultat vide', () => {
-      expect(resultService.getAllResult()).toEqual([]);
-  });
-
-  describe("aprés l'ajout d'un résultat,", () => {
-    beforeEach(() => {
-      const result: ResultModel = {id: 46,idOwner:76,idRecipients:[42],isSeen:false,eventResults:[],contentOfResult:"Test"};
-      resultService = new ResultService();
-      resultService.addResult(result);
-    });
-
-    it('devrait avoir une liste de 1 résultat non vue', () => {
-      expect(resultService.getAllResult().length).toEqual(1);
-    });
-
-    it('devrait avoir une liste de 1 résultat vue aprés la vision de ce résultat', () => {
-      resultService.seenResult(46);
-      expect(resultService.getAllResultSeen().length).toEqual(1);
-      expect(resultService.getAllResult()[1].isSeen).toEqual(true);
-    });
-  });
 
   /* step 2 : 3 resultats */
   describe("aprés l'ajout de 3 resultats,", () => {
@@ -66,29 +44,7 @@ describe('ResultService', () => {
 
 
   /* step 3 (evenement) */
-  describe(",aprés l\'ajout de 3 resultats,", () => {
 
-    beforeEach(() => {
-      // init le service avec 3 resultats (doit etre identique que le step 2)
-    });
-
-    //ps : je ne veux pas que les event de création soi initialisé dans le beforeEach ci dessus mais directement dans le resultService
-    it("devrait avoir la list des résultat dans l\'order de création ( en se basant sur les events de création)", () => {
-      expect(false).toEqual(true);
-    });
-
-    it("devrait avoir 1 event a la date de maintenant quand 1 résultat est vue", () => {
-      expect(false).toEqual(true);
-    });
-
-    it("devrait avoir 2 events avec 2 dates différent aprés la vision d\'un resultat puis la suppression de la vision", () => {
-      expect(false).toEqual(true);
-    });
-
-    it("devrait avoir une fonction qui retourne une liste ordonnée des resultats par rapport au dernier modifier", () => {
-      expect(false).toEqual(true);
-    });
-  });
 
   /* proposé de nouveau test */
 
