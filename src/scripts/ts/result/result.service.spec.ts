d6199a54d71fda06576caa9126f48bcab5644583
matthieu-dev-code <70697534+matthieu-dev-code@users.noreply.github.com>
2022-01-08 16:55:07 +0100

    Update result.service.spec.ts

--- src/scripts/ts/result/result.service.spec.ts
+++ src/scripts/ts/result/result.service.spec.ts
@@ -10,59 +10,7 @@ describe('ResultService', () => {
   });
 
 
-  /* step 1 : initialisation du projet avec 0 et 1 resultat */
-  it('devrait être initialisé avec une liste de résultat vide', () => {
-      expect(resultService.getAllResult()).toEqual([]);
-  });
 
-  describe("aprés l'ajout d'un résultat,", () => {
-    beforeEach(() => {
-      const result: ResultModel = {id: 46,idOwner:76,idRecipients:[42],isSeen:false,eventResults:[],contentOfResult:"Test"};
-      resultService = new ResultService();
-      resultService.addResult(result);
-    });
-
-    it('devrait avoir une liste de 1 résultat non vue', () => {
-      expect(resultService.getAllResult().length).toEqual(1);
-    });
-
-    it('devrait avoir une liste de 1 résultat vue aprés la vision de ce résultat', () => {
-      resultService.seenResult(46);
-      expect(resultService.getAllResultSeen().length).toEqual(1);
-      expect(resultService.getAllResult()[1].isSeen).toEqual(true);
-    });
-  });
-
-  /* step 2 : 3 resultats */
-  describe("aprés l'ajout de 3 resultats,", () => {
-    beforeEach(() => {
-      // init le service avec 3 resultats
-    });
-
-    it("devrait avoir une liste de 3 resultats non vue aprés l\'ajout de 3 resultat.", () => {
-      expect(false).toEqual(true);
-    });
-
-    it("ne devrait pas authorisé l'ajout d'un résultats avec un id existent", () => {
-      expect(false).toEqual(true);
-    });
-
-    it("devrait avoir 1 resultats vue dans la liste aprés la vision d\'un resultat", () => {
-      expect(false).toEqual(true);
-    });
-
-    it("devrait avoir les 3 resultats vue dans la liste aprés qu\'il soit tous vue", () => {
-      expect(false).toEqual(true);
-    });
-
-    it("devrait avoir plus que 2 resultats vue dans la liste aprés qu\'il soit tous vue puis 1 ou la vue est enlevé", () => {
-      expect(false).toEqual(true);
-    });
-
-    it("ne devrait pas planté aprés la vision d\'un resultat non ajouté", () => {
-      expect(false).toEqual(true);
-    });
-  });
 
 
   /* step 3 (evenement) */
